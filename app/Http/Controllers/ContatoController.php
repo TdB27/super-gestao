5de3267d0dc5ebc3dbe5ebb95d08ac63de9d9106
Thiago Billy <thiagobilly27@gmail.com>
2022-12-02 12:01:32 -0300

    validação do formulario e refatoração: parte 1

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,12 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SiteContato;
+use App\Models\MotivoContato;
 
 class ContatoController extends Controller
 {
-    public function contato()
+    public function contato(Request $r)
     {
-        var_dump($_POST);
-        return view('site.contato', ['titulo' => 'Contato']);
+        $motivo_contatos = MotivoContato::all();
+
+        return view('site.contato', [
+            'titulo' => 'Contato',
+            'motivo_contatos' => $motivo_contatos
+        ]);
+    }
+
+    public function salvar(Request $r)
+    {
+        //  realizar a validação dos dados do formulário
+        $r->validate([
+            'nome' => 'required|min:3|max:40',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required|max:2000',
+        ]);
+
+        // SiteContato::create($r->all());
     }
 }
--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\MotivoContato;
 
 class PrincipalController extends Controller
 {
     public function principal()
     {
-        return view('site.principal', ['titulo' => 'Home']);
+        $motivo_contatos = MotivoContato::all();
+
+        return view('site.principal', [
+            'titulo' => 'Home',
+            'motivo_contatos' => $motivo_contatos
+        ]);
     }
 }
